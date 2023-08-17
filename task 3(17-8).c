4cc3810fc0d7ebf7c73fbca836f65ae244589646
Vedantkadiya <142234578+Vedantkadiya@users.noreply.github.com>
2023-08-17 18:51:58 +0530

    Add files via upload

---
+++ task 3(17-8).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   int a,b;
+   printf("enter the value of a:");
+   scanf("%d",&a);
+   printf("enter the value od b:");
+   scanf("%d",&b);
+   if(a==b)
+   {
+       printf("the given number is equal");
+   }
+   else
+   {
+       printf("the given number is not equal");
+   }
+    return 0;
+}
